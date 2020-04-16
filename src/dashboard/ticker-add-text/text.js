215b0cd14010c3df3c95e9a9b055ea46908d96b5
Grant Butler <grantjbutler@users.noreply.github.com>
2020-04-16 19:30:29 -0400

    Clear text field after creating an entry.

--- src/dashboard/ticker-add-text/text.js
+++ src/dashboard/ticker-add-text/text.js
@@ -1,8 +1,12 @@
 import { createInstance } from '../../../index.js'
-            
+
+let textField = document.getElementById('text')
+
 document.addEventListener('dialog-confirmed', function() {
-    let text = document.getElementById('text').value.trim()
+    let text = textField.value.trim()
     if (text.length != 0) {
         createInstance('com.grantjbutler.nodecg-ticker.text', { text })
+
+        textField.value = ''
     }
 });
