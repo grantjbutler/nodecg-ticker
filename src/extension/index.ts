c4aec52586ebf7d68112e152aceceae64ee5dd9d
Grant J. Butler <grantjbutler@users.noreply.github.com>
2023-03-05 11:20:15 -0500

    Update export for the extension.

--- src/extension/index.ts
+++ src/extension/index.ts
@@ -1,11 +1,21 @@
 import NodeCG from "@alvancamp/test-nodecg-types"
 import { set } from '@nodecg-ticker/util/nodecg';
 import { Configschema } from "@nodecg-ticker/types/schemas";
+import { TickerModule } from "./api/ticker-module";
+import { textModule } from "./modules/text";
 
 module.exports = (nodecg: NodeCG.ServerAPI<Configschema>) => {
     set(nodecg);
 
     require('./events');
 
-    return require('./ticker');
+    const Ticker = require('./ticker');    
+
+    return {
+        Ticker,
+        TickerModule,
+        modules: {
+            text: textModule
+        }
+    };
 }
