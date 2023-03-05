02c01d42e437eebaad027df7e833f88fc0164036
Grant J. Butler <grantjbutler@users.noreply.github.com>
2023-03-05 11:25:41 -0500

    Fix ticker import.

--- src/extension/index.ts
+++ src/extension/index.ts
@@ -3,13 +3,12 @@ import { set } from '@nodecg-ticker/util/nodecg';
 import { Configschema } from "@nodecg-ticker/types/schemas";
 import { TickerModule } from "./api/ticker-module";
 import { textModule } from "./modules/text";
+import { Ticker } from "./ticker";
 
 module.exports = (nodecg: NodeCG.ServerAPI<Configschema>) => {
     set(nodecg);
 
     require('./events');
-
-    const Ticker = require('./ticker');    
 
     return {
         Ticker,
