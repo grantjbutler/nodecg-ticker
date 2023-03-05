59922acc6cb965cd69b3fbe184d2624f8aebd665
Grant J. Butler <grantjbutler@users.noreply.github.com>
2023-03-05 12:23:17 -0500

    Specify that the text module has a dialog.

--- src/extension/modules/text.ts
+++ src/extension/modules/text.ts
@@ -1,10 +1,12 @@
 import { TickerModule } from "@nodecg-ticker/api/ticker-module";
+import { get as nodecg } from '@nodecg-ticker/util/nodecg';
 
 export const textModule = new TickerModule<{ text: string }>(
     "com.grantjbutler.ticker-module.text",
     "Text",
     "Displays static text in the ticker."
 )
+.registerDialog('ticker-add-text', nodecg().bundleName)
 .onCreateInstance(instance => {
     instance.name = "Text";
     instance.description = instance.data.text;
