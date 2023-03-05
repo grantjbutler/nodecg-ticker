371b7970621fea223acbfbb37dee47e65df13a53
Grant J. Butler <grantjbutler@users.noreply.github.com>
2023-03-05 11:57:54 -0500

    Fix typings from making registry replicant ephemeral.

--- src/extension/util/replicants.ts
+++ src/extension/util/replicants.ts
@@ -2,5 +2,5 @@ import { Ticker, TickerModuleRegistry } from '@nodecg-ticker/types/schemas';
 import { get as nodecg } from './nodecg';
 
 export const currentTickerReplicant = nodecg().Replicant('current-ticker', { persistent: false });
-export const tickerModuleRegistryReplicant = nodecg().Replicant<TickerModuleRegistry>('ticker-module-registry', { persistent: false });
+export const tickerModuleRegistryReplicant = nodecg().Replicant<TickerModuleRegistry>('ticker-module-registry', { persistent: false, defaultValue: [] });
 export const tickerReplicant = nodecg().Replicant<Ticker>('ticker');
