74ff04fadb1df56edd2b8b95f0dc724b1831c9cb
Grant J. Butler <grantjbutler@users.noreply.github.com>
2023-03-22 21:42:17 -0400

    Fix spacing.

--- src/dashboard/components/Ticker.vue
+++ src/dashboard/components/Ticker.vue
@@ -49,7 +49,7 @@ onMounted(() => {
                 <div class="flex-1 p-2">
                     {{ item.description }}
                 </div>
-                <div class="flex hidden group-hover:block no-hover:block pointer-coarse:gap-2 pr-2">
+                <div class="hidden group-hover:flex no-hover:flex pointer-coarse:gap-2 pr-2">
                     <button class="w-6 h-8 text-gray-500 disabled:opacity-25" :disabled="index == ticker.length - 1" @click="moveDown(item.id)">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" class="object-fill fill-current"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
                     </button>
